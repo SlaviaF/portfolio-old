7c31f9e1ca0b6e3693d34d9d20a86df396c9ad46
SlaviaF <slaviafurtado@gmail.com>
2022-02-11 09:51:45 +0100

    project page modified

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,52 @@
-import React from 'react'
-import projectsData from './projectData';
+import React from "react";
+import projectsData from "./projectData";
 
 const Projects = () => {
   return (
-    <div id = "projects"className=" common-animation">
+    <div id="projects" className=" common-animation">
       <div className="container">
-          <div className="common">
-            <h3 className="heading">PROJECTS</h3>
-            <h1 className="mainHeader">My Projects</h1>
-            <div className="commonBorder"></div>
-          </div>
-          <div className = "row">
-            {projectsData.map(project => {
-              return(
-                <div className="card" style={{width: "50rem", margin:"2em auto", padding:"1em 1em", border:"2px solid darkgrey"}}>
-                <img src={project.img} className="card-img-top" alt={project.title}/>
-                <div className="card-body">
-                <p className="card-text"> <a href={project.source} target="_blank">Source  code</a> <div><p>{project.title}</p></div></p>
-                </div>
-            </div>
-              )
-            })}
-            </div>
-            </div>
+        <div className="common">
+          <h3 className="heading">PROJECTS</h3>
+          <h1 className="mainHeader">My Projects</h1>
+          <div className="commonBorder"></div>
         </div>
+        <div className="row">
+          {projectsData.map((project) => {
+            return (
+              <div
+                className="card"
+                style={{
+                  width: "50em",
+                  margin: "2em auto",
+                  padding: "1em 1em",
+                  border: "2px solid darkgrey",
+                }}
+              >
+                <img
+                  src={project.img}
+                  className="card-img-top"
+                  alt={project.title}
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    <a href={project.source} target="_blank">
+                      Source code
+                    </a>
+                    <br/>
+                    <a href={project.url} target="_blank">
+                      View Live
+                    </a>
+                    <div>
+                      <p>{project.title}</p>
+                    </div>
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 };
 
